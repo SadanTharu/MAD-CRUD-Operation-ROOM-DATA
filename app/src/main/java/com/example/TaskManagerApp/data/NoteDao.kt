79c8f56a9c748a9d3ae0b65a33a6d6dfd097f097
Tharushika Sadan <143350661+SadanTharu@users.noreply.github.com>
2024-05-12 16:05:05 +0530

    note variables

---
+++ app/src/main/java/com/example/TaskManagerApp/data/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.example.TaskManagerApp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.TaskManagerApp.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("DELETE FROM note_table")
+    suspend fun deleteAllNotes()
+
+    @Query("SELECT * FROM note_table ORDER BY priority ASC")
+    fun readAllData(): LiveData<List<Note>>
+
+}
